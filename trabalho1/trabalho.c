f8cc256a0559bec54c01984a0dd7976b262e2ee0
Giordano Silveira <giordanovraau0204@gmail.com>
2021-02-15 22:48:03 -0300

    Makefile e comentários prontos

--- trabalho1/trabalho.c
+++ trabalho1/trabalho.c
@@ -10,25 +10,13 @@ struct t_node {
 };
 typedef struct t_node t_node;
 
-int *allocate_menI (int n) {
-
-	int *vetor;
-
-	vetor = malloc(sizeof(int)*n);
-    if (!vetor) {
-        perror ("Não foi possível alocar memória");
-        exit (1);
-    }
-
-	return vetor;
-
-}
+//Aloca espaço na memória para um vetor de char
 char *allocate_men (int n) {
 
 	char *vetor;
 
 	vetor = malloc(sizeof(char)*n);
-    if (!vetor) {
+    if (!vetor) {                                           //Testa para ver se foi possível alocar espaço na memória
         perror ("Não foi possível alocar memória");
         exit (1);
     }
@@ -36,19 +24,23 @@ char *allocate_men (int n) {
 	return vetor;
 }
 
+//Cria um nó da árvore e retorna ele 
 t_node *create_node (const char *numero) {
 	
 	t_node *new;
-	new = (t_node*)malloc(sizeof(t_node));
 
+    //Aloca espaço e testa para ver se alocou
+	new = (t_node*)malloc(sizeof(t_node));
     if (!new) {
         perror ("Não foi possível alocar memória");
         exit (1);
     }
 
+    //Aloca espaço para o vetor de string da estrutura
 	new->key = allocate_men(MAX);
     strcpy (new->key, numero);
-    	
+
+    //Inicializa o ponteiro da esquerda, direita e pai como NULL	
 	new->left = NULL;
 	new->right = NULL;
 	new->pai = NULL;
@@ -56,29 +48,41 @@ t_node *create_node (const char *numero) {
 	return new;
 
 }
+
+//Recebe como parametro uma expressão de parênteses alinhados e monta a árvore;
+//Retorna a árvore montada
 t_node *monta_arvore(char *expr, int *i){
 
     int j;
     char *num;
 
+    //Espaço para o vetor auxiliar que vai receber os números e as operações
     num = allocate_men (MAX);
 
 	t_node *tree;
 	tree = NULL;
+
+    //Testa para ver se é um parênteses aberto
+    //Ou seja, se vai precisar criar um novo nodo
 	if (expr[*i] == '(') {
 		
         (*i)++;
-        j = 0;
-        while (expr[*i] != ')' && expr[*i] != '(') {
+        j = 0;                                              //índice para o vetor num
+
+        //Enquanto não é uma parênteses fechado (fim de um nó) ou aberto (começo de outro nó),
+        //copia esses caracteres para o vertor num
+        while (expr[*i] != ')' && expr[*i] != '(') {    
             num[j] = expr[*i];
             j++;
             (*i)++;
         }
         num[j] = '\0';
 
+        //Mostra qual nó vai ser criado e cria esse nó
 		printf("Criando nó %s\n", num);
 		tree = create_node (num);
 
+        //Monta primeiramente a árvore da esquerda e depois a direita
 		tree->left = monta_arvore(expr,i);
 		tree->right = monta_arvore(expr,i);
 		(*i)++;
@@ -86,27 +90,30 @@ t_node *monta_arvore(char *expr, int *i){
 	return tree;
 	
 }
-/*void posordem(t_node *no){
-    if (no != NULL){    
-        posordem(no->left);        
-        printf ("%s\n", no->key);
-        posordem(no->right);
-    }
-}*/
+
+//Anda de maneira "pos-ordem" arvore e coleta o resultado das operações
+//Retorna o resultado da conta
 long avalia_expressao (t_node* raiz) {
 
 	int r_valor, l_valor;
     char *ptr;
 
+    //Checa para ver se o nó é válido
 	if (raiz == NULL)
 		return 0;
 
+    //Checa para ver se é um número
+    //É um número se ambos filhos forem nulos, ou seja, se é um nó folha
 	if (!raiz->left && !raiz->right)
 		return strtol (raiz->key, &ptr, 10);
 	
+    //Recursivamente vai montando as operações
+    //Vem da última sub-árvore da esquerda
 	l_valor = avalia_expressao (raiz->left);
 	r_valor = avalia_expressao (raiz->right);
 
+    //Volta a chamada da recursão exatamente no nó do operador
+    //É só checar para ver qual é a operação correspondente e efetuar essa operação
 	if (strcmp(raiz->key,"+") == 0)
 		return l_valor + r_valor;
 
@@ -116,6 +123,7 @@ long avalia_expressao (t_node* raiz) {
 	if (strcmp(raiz->key,"*") == 0)
 		return l_valor * r_valor;
 		
+    //A divisão merece uma atenção a mais por causa da divisão por 0
 	if (strcmp(raiz->key,"/") == 0) {
 		if (r_valor == 0) {
 			printf ("Valor inválido - divisão por 0 - encerrando programa\n");
@@ -132,13 +140,18 @@ int main () {
 	t_node *arvore;
 
 	int i = 0;
+
+    //Aloca espaço na memória para o vetor da expressão
 	expressao = allocate_men (MAX);
 	
+    //Lê a expressão de parenteses alinhados do teclado
 	fgets (expressao, MAX, stdin);
 
+    //monta a árvore
 	arvore = monta_arvore(expressao, &i);
     printf ("\n");
-    
+
+    //Calcula o resultado da árvore
 	resultado = avalia_expressao (arvore);
 
 	printf ("Resultado = %ld\n", resultado);
